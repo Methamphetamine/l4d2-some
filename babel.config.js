ceaedf4116a0483bf87c2a217d0a7237edf27517
Jackson Gray <GanibalSlash@yandex.ru>
2025-06-15 07:16:35 +0300

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -3,3 +3,14 @@ module.exports = {
     '@vue/cli-plugin-babel/preset'
   ]
 }
+module.exports = function (api) {
+  api.cache(true);
+
+  const presets = [ '@vue/cli-plugin-babel/preset' ];
+  const plugins = [ '@babel/plugin-syntax-import-attributes' ];
+
+  return {
+    presets,
+    plugins
+  };
+}
